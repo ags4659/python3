3ed64ba335a3a3731e027aff3a5025b3755b02a6
ags4659 <ags4659@gmail.com>
2022-07-26 20:27:13 +1000

    update

--- arguments.py
+++ arguments.py
@@ -1,1 +1,4 @@
-print("Hi ")
+#print("Hi ","i'm","giri" '\tthis is with single quote \n',"\t'this is with I'm'", sep="--", end="\b")
+#print ("thisis seconf line joined with end")
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python")
