ed4ceff6920ee2981f1dbc0a93b0eaa189eb7e5a
ags4659 <ags4659@gmail.com>
2023-03-03 19:11:06 +1100

    with notes

--- ytdown/ytd.py
+++ ytdown/ytd.py
@@ -4,4 +4,5 @@ from sys import argv
 link = argv[1]
 yt = YouTube(link)
 yd = yt.streams.get_highest_resolution()
-yd.download('/YoutubeDowloads')
+yd.download('/homyte/ags/Downloads/youdownloads')
+## This programme needs an argument for example "python3 ytd.py "youtube link"
